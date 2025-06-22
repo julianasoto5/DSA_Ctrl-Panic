ddad587bca771a6b31654cc792fdbfe49eea7ec2
julianasoto5 <115961804+julianasoto5@users.noreply.github.com>
2025-06-22 19:33:16 -0300

    app

---
+++ tpi_python/www/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect, url_for
+import random
+import string
+import base64
+import os
+
+app = Flask(__name__)
+clubes = ["Estudiantes de La Plata", "Boca Juniors", "River Plate", "Racing Club", "Independiente", "San Lorenzo", "Vélez Sarsfield", "Newell"]
+    
+def generar_contrasena(longitud):
+    caracteres = string.ascii_letters + string.digits
+    contrasena = ''.join(random.choice(caracteres) for i in range(longitud))
+    return contrasena
+
+def elegir_club():
+    return random.choice(clubes)
+
+USUARIOS = [{"id": 1, "usuario": "user", "contrasena": "pass", "apellido": "Panic", "nombre": "Ctrl", "club": clubes[0]},
+            {"id": random.randint(31,100), "usuario": "admin", "contrasena": generar_contrasena(8), "apellido": "Tengo la flag", "nombre": "Flagger", "club": os.environ.get("FLAG")}]
+
+for i in range(1,30):
+    usuario = {}
+    usuario["id"] = i+1
+    usuario["usuario"] = "usuario" + str(i + 1)
+    usuario["contrasena"] = generar_contrasena(8)
+    usuario["apellido"] = "Apellido" + str(i + 1)
+    usuario["nombre"] = "Nombre" + str(i + 1)
+    usuario["club"] = elegir_club()
+    USUARIOS.append(usuario)
+
+# Renderiza la página de inicio
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Renderiza la página de inicio de sesión
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        print("Se recibió un POST")
+
+        usuario = request.form["usuario"]
+        contrasena = request.form["contrasena"]
+        if usuario == "admin":
+            print(contrasena)
+        for usuario_bd in USUARIOS:
+            if usuario_bd["usuario"] == usuario and usuario_bd["contrasena"] == contrasena:
+                return redirect(url_for("perfil", id=usuario_bd["id"]))
+        error = "Invalid credentials"
+    return render_template("index.html", error=error)
+
+# Renderiza la página de perfil
+@app.route("/perfil/<int:id>", methods=["GET", "POST"])
+def perfil(id):
+    usuario_bd = None
+    for usuario in USUARIOS:
+        if usuario["id"] == id:
+            usuario_bd = usuario
+            break
+    if usuario_bd is None:
+        return redirect(url_for("index"))
+
+    if request.method == "POST":
+        usuario_bd["nombre"] = request.form["nombre"]
+        usuario_bd["club"] = request.form["club"]
+        return redirect(url_for("perfil", id=id))
+    return render_template("perfil.html", usuario=usuario_bd)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
