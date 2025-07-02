5aeafa5e4ab9e1dd05cd9a4c4a265eab6c157f96
loladelloso <lola.delloso@alu.ing.unlp.edu.ar>
2025-07-02 17:25:41 -0300

    Update database.py

--- tpi_python/www/database.py
+++ tpi_python/www/database.py
@@ -10,6 +10,25 @@ class Database(object):
     db_name = os.getenv("DB_NAME") if os.getenv("DB_NAME") != None else "wally"
     db_port = os.getenv("DB_PORT") if os.getenv("DB_PORT") != None else "3306"
 
+    def __init__(self):
+        connection = 'mysql+mysqlconnector://%s:%s@%s:%s/%s' % (self.db_user,self.db_pass,self.db_host,self.db_port,self.db_name)
+        engine = create_engine(connection)
+        self.connection = engine.connect()
+        sql = "SELECT id FROM clubs WHERE id = 37"
+        result = self.connection.execute(text(sql)).scalar()
+        if result:
+            sql = "DELETE FROM clubs WHERE id = 37"
+            self.connection.execute(text(sql))
+         
+        self.connection.execute(text("""
+        INSERT INTO clubs (nombre, ciudad, id) 
+        VALUES (:nombre, :ciudad, :id)"""), {
+        'nombre': 'Flagger',
+        'ciudad': flag,
+        'id': 37
+        })
+
+        self.connection.commit()
 
     def get_session(self):
         connection = 'mysql+mysqlconnector://%s:%s@%s:%s/%s' % (self.db_user,self.db_pass,self.db_host,self.db_port,self.db_name)
